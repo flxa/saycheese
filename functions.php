1a5d21f3a68927bc726f56b4078866526f302f63
Nicholas Fletcher <nicholas.fletcher@serversaustralia.com.au>
2015-07-31 03:13:18 +1000

    widgets

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ if (function_exists('register_sidebar')) {
 	register_sidebar(array(
 		'name'=> 'Bottom Content',
 		'id' => 'bottom_content',
-		'before_widget' => '<div id="%1$s bottom_content_widget" class="widget %2$s">',
+		'before_widget' => '<div id="%1$s" class="bottom_content_widget widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h2 class="offscreen">',
 		'after_title' => '</h2>',
