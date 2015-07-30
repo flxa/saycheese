5921a7a4737ef767c6fc2e48d658b70c0e458bcd
Nicholas Fletcher <nicholas.fletcher@serversaustralia.com.au>
2015-07-31 04:48:49 +1000

    finishing last tweaks

--- footer.php
+++ footer.php
@@ -3,6 +3,12 @@
 	</div><!-- .site-content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<h1 class="entry-title" style="
+			position: absolute;
+			right: 1em;
+			background-color: #DE5D8D;
+			font-size: 2em;
+			transform: skew(10deg) rotate(10deg);">It’s all about the fun</h1>
 		<div class="site-info">
 			<?php
 				/**
