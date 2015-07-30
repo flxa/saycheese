73b64e28686793b5743443ab787841b3c32137f9
Nicholas Fletcher <nicholas.fletcher@serversaustralia.com.au>
2015-07-30 21:21:48 +1000

    add the theme files solves #1

---
+++ footer.php
@@ -0,0 +1,22 @@
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php
+				/**
+				 * Fires before the Twenty Fifteen footer text for footer customization.
+				 *
+				 * @since Twenty Fifteen 1.0
+				 */
+				do_action( 'twentyfifteen_credits' );
+			?>
+			<h6><a href="/"><span class="two-ex">&copy;</span> Copyright <?php echo date('Y'); ?> Hunter Wedding Grapevine</a></h6>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
