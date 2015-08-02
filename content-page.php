0a74bc1979fe0794079f623dc3487d4574402b5f
Nicholas Fletcher <nicholas.fletcher@serversaustralia.com.au>
2015-08-02 21:47:23 +1000

    testing home page call

--- content-page.php
+++ content-page.php
@@ -15,7 +15,11 @@
 	?>
 
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php 
+		if ( is_front_page() === '') {	
+			the_title( '<h1 class="entry-title">-', '_'.is_front_page().'-</h1>' );
+		}
+		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
